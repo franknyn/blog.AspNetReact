45386654d0b8ba9d3499a6a522f45e63bf7755a1
Frank <franknyn@yahoo.com>
2020-04-05 02:27:03 +1000

    updated react bundle config

--- blog.AspNetReact/App_Start/BundleConfig.cs
+++ blog.AspNetReact/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace blog.AspNetReact
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/react-bundle").Include(
+                      "~/Scripts/dist/Home/react/bundle.js"));
         }
     }
 }
